c4f4a00bb3069a14a78949dbb720ad0ab125cd25
Aruldharani <36417297+Aruldharani@users.noreply.github.com>
2019-02-27 11:15:51 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int t;
+scanf("%d",&t);
+int n;
+scanf("%d",&n);
+if(n==0)
+{
+printf("Zero\n");
+}
+if(n>0)
+{
+printf("Positive\n");
+}
+if(n<0)
+{
+printf("Negative\n");
+}
+}
